5009e4cf3d07aa02270c64b98e3010dedf240585
VitalyCherkov <vv-ch@bk.ru>
2019-01-04 16:00:40 +0300

    removed old code

--- main.go
+++ main.go
@@ -8,109 +8,6 @@ import (
 	"strings"
 )
 
-//type FileTree struct {
-//	children []FileTree
-//	info os.FileInfo
-//}
-
-//// Добавляет разделенный по кускам путь в дерево
-//func (ft *FileTree) appendSpread(pathChunks []string, info os.FileInfo) error {
-//	if len(pathChunks) == 0 {
-//		return fmt.Errorf("incorrect path: %s", strings.Join(pathChunks, "/"))
-//	}
-//
-//	currentPos := -1
-//	for index, val := range ft.children {
-//		if val.info.Name() == pathChunks[0] {
-//			currentPos = index
-//		}
-//	}
-//
-//	if currentPos == -1 {
-//		if len(pathChunks) == 1 {
-//			ft.children = append(ft.children, FileTree{
-//				info:info,
-//			})
-//			return nil
-//		} else {
-//			return fmt.Errorf("incorrect path: %s", strings.Join(pathChunks, "/"))
-//		}
-//	}
-//
-//	return ft.children[currentPos].appendSpread(pathChunks[1:], info)
-//}
-//
-//// Разделяет путь и удаляет из него начальную часть, метод выше
-//func (ft *FileTree) Append(entryPath string, path string, info os.FileInfo) error {
-//	entryPathChunks := strings.Split(entryPath, string(filepath.Separator))
-//	pathChunks := strings.Split(path, string(filepath.Separator))
-//
-//	if len(pathChunks) == 0 || len(entryPathChunks) == 0 {
-//		return errors.New("empty string")
-//	}
-//
-//	if entryPathChunks[0] == "." {
-//		entryPathChunks = entryPathChunks[1:]
-//	}
-//	if pathChunks[0] == "." {
-//		pathChunks = pathChunks[1:]
-//	}
-//
-//	pathChunks = pathChunks[len(entryPathChunks):]
-//
-//	if len(pathChunks) > 0 {
-//		return ft.appendSpread(pathChunks, info)
-//	}
-//	return nil
-//}
-//
-//// Формирует имя файла
-//func (ft *FileTree) getFileName() string {
-//	if ft.info == nil {
-//		return ""
-//	}
-//
-//	if ft.info.IsDir() {
-//		return ft.info.Name()
-//	}
-//
-//	if ft.info.Size() > 0 {
-//		return fmt.Sprintf("%s (%db)", ft.info.Name(), ft.info.Size())
-//	}
-//	return fmt.Sprintf("%s (empty)", ft.info.Name())
-//}
-//
-//// Преробразует дерево к массиву строк
-//func (ft *FileTree) toTreeStrings(prefix string, isLast bool) []string {
-//	ownPrefix := "├───"
-//	if isLast {
-//		ownPrefix = "└───"
-//	}
-//
-//	var result = []string{prefix + ownPrefix + ft.getFileName()}
-//
-//	childPrefix := "│\t"
-//	if isLast {
-//		childPrefix = "\t"
-//	}
-//
-//	for index, child := range ft.children {
-//		result = append(result, child.toTreeStrings(prefix + childPrefix, index == len(ft.children) - 1)...)
-//	}
-//
-//	return result
-//}
-//
-//// Формирует вывод
-//func (ft *FileTree) String() string {
-//	var result []string
-//	for index, subtree := range ft.children {
-//		result = append(result, subtree.toTreeStrings("", index == len(ft.children) - 1)...)
-//	}
-//
-//	return strings.Join(result, "\n")
-//}
-
 func printPrefix(out io.Writer, prefix []bool, meIsLast bool) error {
 	for _, isLastInLevel := range prefix {
 		var err error
@@ -134,8 +31,7 @@ func printPrefix(out io.Writer, prefix []bool, meIsLast bool) error {
 }
 
 func printFileName(out io.Writer, file os.FileInfo) error {
-	var err error
-	_, err = fmt.Fprint(out, file.Name());
+	_, err := fmt.Fprint(out, file.Name());
 	if err != nil {
 		return err
 	}
@@ -151,9 +47,7 @@ func printFileName(out io.Writer, file os.FileInfo) error {
 }
 
 func printFullFile(out io.Writer, prefix []bool, meIsLast bool, file os.FileInfo) error {
-	var err error
-
-	err = printPrefix(out, prefix, meIsLast)
+	err := printPrefix(out, prefix, meIsLast)
 	if err != nil {
 		return err
 	}
@@ -170,8 +64,7 @@ func printFullFile(out io.Writer, prefix []bool, meIsLast bool, file os.FileInfo
 
 func getNextIndex(files []os.FileInfo, curIndex int, printFiles bool) (nextIndex int, hasNext bool) {
 	if curIndex >= len(files) || len(files) == 0 {
-		hasNext = false
-		return
+		return nextIndex, false
 	}
 
 	nextIndex = curIndex + 1;
@@ -180,37 +73,21 @@ func getNextIndex(files []os.FileInfo, curIndex int, printFiles bool) (nextIndex
 		if curIndex < len(files) - 1 {
 			hasNext = true
 		}
-		return
+		return nextIndex, hasNext
 	}
 
 	for ; nextIndex < len(files); nextIndex++ {
 		if files[nextIndex].IsDir() {
-			hasNext = true
-			return
+			return nextIndex, true
 		}
 	}
 
-	nextIndex = len(files)
-	hasNext = false
-	return
+	return nextIndex, false
 }
 
-func dirTreeRec(out io.Writer, path string, file os.FileInfo, prefix []bool, meIsLast bool, printFiles bool, printMe bool) error {
-	if !file.IsDir() && !printFiles {
-		return fmt.Errorf("file [%s] is not dir", file.Name())
-	}
-
-	var err error
-
-	if printMe {
-		err = printFullFile(out, prefix, meIsLast, file)
-		if err != nil {
-			return err
-		}
-	}
-
+func dirTreeRec(out io.Writer, path string, file os.FileInfo, prefix []bool, printFiles bool) error {
 	if !file.IsDir() {
-		return nil
+		return fmt.Errorf("file [%s] is not dir", file.Name())
 	}
 
 	files, err := ioutil.ReadDir(path)
@@ -228,14 +105,18 @@ func dirTreeRec(out io.Writer, path string, file os.FileInfo, prefix []bool, meI
 		nextIndex, hasNext = getNextIndex(files, nextIndex, printFiles) {
 
 		nextPath := strings.Join([]string{path, files[curIndex].Name()}, string(os.PathSeparator))
-		nextPrefix := prefix
-		if printMe {
-			nextPrefix = append(prefix, meIsLast)
-		}
 
-		err = dirTreeRec(out, nextPath, files[curIndex], nextPrefix, !hasNext, printFiles, true)
-		if err != nil {
-			return err
+		if files[curIndex].IsDir() || printFiles {
+			err := printFullFile(out, prefix, !hasNext, files[curIndex])
+			if err != nil {
+				return err
+			}
+		}
+		if files[curIndex].IsDir() {
+			err := dirTreeRec(out, nextPath, files[curIndex], append(prefix, !hasNext), printFiles)
+			if err != nil {
+				return err
+			}
 		}
 
 		curIndex = nextIndex
@@ -250,32 +131,8 @@ func dirTree(out io.Writer, path string, printFiles bool) error {
 		return err
 	}
 
-	return dirTreeRec(out, path, file, nil, false, printFiles, false)
+	return dirTreeRec(out, path, file, nil, printFiles)
 }
-
-//func dirTree2(out io.Writer, path string, printFiles bool) error {
-//
-//	var ft FileTree
-//
-//	err := filepath.Walk(path, func(curPath string, info os.FileInfo, err error) error {
-//		if err != nil {
-//			return err
-//		}
-//
-//		if info.IsDir() || printFiles {
-//			return ft.Append(path, curPath, info)
-//		}
-//		return nil
-//	})
-//
-//	if err != nil {
-//		return err
-//	}
-//
-//	fmt.Fprintln(out, ft.String())
-//
-//	return nil
-//}
 
 func main() {
 	out := os.Stdout
